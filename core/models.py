618558fda146484c7dfe316e80e71585a5592f5d
Lê Tiến Minh <minh-4T@users.noreply.huggingface.co>
2026-03-28 17:44:00 +0000

    exit llm to qa_pipeline

--- core/models.py
+++ core/models.py
@@ -1,24 +1,11 @@
-from langchain_ollama import OllamaLLM
 from langchain_huggingface import HuggingFaceEmbeddings
-from sentence_transformers import CrossEncoder
-from .config import LLM_MODEL, EMBED_MODEL, CROSS_ENCODER_MODEL, GROQ_API_KEY
-from langchain_groq import ChatGroq
+from .config import EMBED_MODEL
 
-print(" Đang khởi tạo các models...")
-llm  = ChatGroq(
-    model=LLM_MODEL,
-    groq_api_key=GROQ_API_KEY,
-    temperature=0.2,
-)
-
-
-print(f" Đang tải Cross-Encoder: {CROSS_ENCODER_MODEL}")
-cross_encoder = CrossEncoder(CROSS_ENCODER_MODEL)
-
+# Model này sẽ chạy trên CPU của Hugging Face
 embeddings = HuggingFaceEmbeddings(
     model_name=EMBED_MODEL,
     model_kwargs={'device': 'cpu'},
-    encode_kwargs={'normalize_embeddings': True, 'batch_size': 128}
+    encode_kwargs={'normalize_embeddings': True}
 )
 
-print(" Hoàn tất khởi tạo models!")
+llm = None
