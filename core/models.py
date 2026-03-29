c6458da8fb66ab994d5b20aa2a3f19d6e64ae279
Lê Tiến Minh <minh-4T@users.noreply.huggingface.co>
2026-03-29 02:23:50 +0000

    Update core/models.py

--- core/models.py
+++ core/models.py
@@ -1,12 +1,14 @@
 from langchain_huggingface import HuggingFaceEmbeddings
-from .config import EMBED_MODEL
+from sentence_transformers import CrossEncoder 
+from .config import EMBED_MODEL, CROSS_ENCODER_MODEL
 
-# Model này sẽ chạy trên CPU của Hugging Face
+# Khởi tạo Embedding model - Chạy trên CPU của Hugging Face
 embeddings = HuggingFaceEmbeddings(
     model_name=EMBED_MODEL,
     model_kwargs={'device': 'cpu'},
     encode_kwargs={'normalize_embeddings': True}
 )
+
 cross_encoder = CrossEncoder(CROSS_ENCODER_MODEL, device='cpu')
 
 llm = None
